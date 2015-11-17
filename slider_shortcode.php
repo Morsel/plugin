1800305c5be73393a3117256b308fe6c1c49c0c6
nishant <nishant.n@cisinlabs.com>
2015-11-17 15:03:56 +0530

    fix issue for server on plugin in slider

--- slider_shortcode.php
+++ slider_shortcode.php
@@ -1,6 +1,6 @@
 <?php
 add_action('wp_enqueue_scripts', 'sliderFrontendScripts');
-function sliderFrontendScripts() {  
+function sliderFrontendScripts() {
   if(!is_admin()){
     wp_enqueue_style('shs-front',plugins_url('admin/slider/css/shs-front.css',__FILE__));
     // wp_enqueue_script('shs-front-script',plugins_url('admin/slider/js/jquery-1.9.1.min.js',__FILE__));
@@ -9,7 +9,7 @@ function sliderFrontendScripts() {
 }
 
 function morselDisplaySlider()
-{  
+{
 
   $shs_settings=get_option('morselSliderSettings');
   $slider_duration=$shs_settings['pause_time'];
@@ -19,22 +19,22 @@ function morselDisplaySlider()
   // $direction=$shs_settings['direction'];
   $autoplay=$shs_settings['show_navigation'];
 
-  ?>  
+  ?>
   <div style="width:<?=$width;?> ; margin:0 auto;">
     <div id="jssor_1" style="position: relative; margin: 0 auto; top: 0px; left: 0px; width: 1300px; height: 500px; overflow: hidden; visibility: hidden;">
         <!-- Loading Screen -->
-      
+
         <div data-u="loading" style="position: absolute; top: 0px; left: 0px;">
             <div style="filter: alpha(opacity=70); opacity: 0.7; position: absolute; display: block; top: 0px; left: 0px; width: 100%; height: 100%;"></div>
             <div class="sliderAboveDiv"></div>
         </div>
         <div data-u="slides" style="cursor: default; position: relative; top: 0px; left: 0px; width: 1300px; height: 500px; overflow: hidden;">
-            <? 
+            <?
 
             $contents = get_option('shs_slider_contents');
-           
+
             foreach ($contents as $key => $value) {
-             
+
             ?>
             <div data-p="225.00" style="display: none;">
                 <img data-u="image" src="<?=$value?>" />
@@ -51,12 +51,12 @@ function morselDisplaySlider()
         <span data-u="arrowright" class="jssora22r" style="top:123px;right:12px;width:40px;height:58px;" data-autocenter="2"></span>
     </div>
   </div>
-    
-    
+
+
     <!-- use jssor.slider.debug.js instead for release -->
     <script>
         jQuery(document).ready(function ($) {
-            
+
             var jssor_1_options = {
               $AutoPlay: <?=$autoplay;?>,
               $SlideDuration: <?=$slider_duration;?>,
@@ -68,9 +68,9 @@ function morselDisplaySlider()
                 $Class: $JssorBulletNavigator$
               }
             };
-            
+
             var jssor_1_slider = new $JssorSlider$("jssor_1", jssor_1_options);
-            
+
             //responsive code begin
             //you can remove responsive code if you don't want the slider scales while window resizes
             function ScaleSlider() {
@@ -93,4 +93,4 @@ function morselDisplaySlider()
 
 <? } //function end
   add_shortcode('morselDisplaySlider', 'morselDisplaySlider');
-?>  
+
